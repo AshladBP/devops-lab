8fdf9e5a61b54627e10c89be76743765c518d56c
Liixray <koda16.77@gmail.com>
2025-10-31 15:15:59 +0100

    Add infrastructure for CD

--- td5/scripts/tofu/live/tofu-state/backend.tf
+++ td5/scripts/tofu/live/tofu-state/backend.tf
@@ -1,9 +1,9 @@
-#terraform {
-#    backend "s3" {
-#        bucket = "unique-devops-bucket-name" # Replace
-#        key = "td5/scripts/tofu/live/tofu-state"
-#        region = "us-east-2" # Your AWS region
-#        encrypt = true
-#        dynamodb_table = "unique-devops-dynamodb-table" # Replace
-#    }
-#}
+terraform {
+    backend "s3" {
+        bucket = "unique-devops-bucket-name" # Replace
+        key = "td5/scripts/tofu/live/tofu-state"
+        region = "us-east-2" # Your AWS region
+        encrypt = true
+        dynamodb_table = "unique-devops-dynamodb-table" # Replace
+    }
+}
