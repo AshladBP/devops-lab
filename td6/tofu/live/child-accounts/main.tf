b79946b16a03f359050c72bb92646474651b89c2
Liixray <koda16.77@gmail.com>
2025-11-04 17:41:33 +0100

    Adding child accounts

---
+++ td6/tofu/live/child-accounts/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-2" # Or your preferred region
+}
+module "child_accounts" {
+
+    source = "github.com/AshladBP/devops-lab//td6/tofu/modules/aws-organization"
+    create_organization = true # Set to false if you already enabled AWS Organizations
+    dev_account_email = "nassim.mahnane+dev@edu.esiee.fr" # Replace with your email alias
+    stage_account_email = "nassim.mahnane+stage@edu.esiee.fr" # Replace with your email alias
+    prod_account_email = "nassim.mahnane+prod@edu.esiee.fr" # Replace with your email alias
+}
